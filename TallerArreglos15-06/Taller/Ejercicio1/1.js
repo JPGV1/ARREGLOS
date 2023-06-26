9198186bec28c18a526796955a6fbfc23d5703b3
Juan Pablo García Villa <juanpablogarciavilla2005@gmail.com>
2023-06-26 15:58:47 -0500

    Avance a el ejercicio11 de miscelanea

---
+++ TallerArreglos15-06/Taller/Ejercicio1/1.js
@@ -0,0 +1,29 @@
+
+/*a*/
+function A() {
+    let arreglo =[1,2,3,4,5,6]
+    while (true) {
+        alert(arreglo[0]);
+        alert(arreglo[1]);
+        alert(arreglo[2]);
+        alert(arreglo[3]);
+        alert(arreglo[4]);
+        alert(arreglo[5]);
+
+    }
+}
+
+/*b*/
+function B() {
+    let arreglo =[1,2,3,4,5,6]
+    for (let index = 0; index < arreglo.length; index++) {
+        console.log(arreglo[index]);
+        
+    }
+}
+
+/*C*/
+function C() {
+    let arreglo =[1,2,3,4,5,6]
+    
+}
