c17fee416025e7fd914d4419483ba16165420081
JPGV1 <126621502+JPGV1@users.noreply.github.com>
2023-06-29 15:01:05 -0500

    Add files via upload

---
+++ teoria/longitud/longitud.js
@@ -0,0 +1,10 @@
+//LONGITUD DE UN ARREGLO
+//ES EL NUMERO DE ELEMENTOS DEL ARREGLO
+
+let arreglo = [1, 4, 5, 9];
+console.log("Longitud: ", arreglo.length);
+
+let arregloBidimensional = [[5, 6, 8], [0]];
+console.log("Longitud bidi: ", arregloBidimensional.length);
+
+console.log("Longitud bidi primer ele: ", arregloBidimensional[0].length);
